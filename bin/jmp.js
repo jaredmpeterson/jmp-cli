3f3de106f6d3d465fab15a00454eec9957d89223
Jared M. Peterson <jared@peteyinc.com>
2018-06-14 14:52:38 -0600

    add LoginManager

--- bin/jmp.js
+++ bin/jmp.js
@@ -1,3 +1,11 @@
 #! /usr/bin/env node
 
-console.log('Hello, my name is Jared!');
+const LoginManager = require('../lib/login-manager');
+
+async function main() {
+  const login = new LoginManager('jmp-cli');
+  let [user, email] = await login.getUserAndEmail();
+  console.log(`Hello ${user}, my name is Jared!`);
+}
+
+main().catch(console.error);
