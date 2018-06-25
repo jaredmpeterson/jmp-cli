703956dd7a6ab5bc4bedaa23cbabdeb7ac648695
Jared M. Peterson <developer@peteyinc.com>
2018-06-25 16:00:28 -0600

    added slack command

---
+++ bin/jmp-slack.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+const program = require("commander")
+const { IncomingWebhook } = require("@slack/client")
+const url =
+  "https://hooks.slack.com/services/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"
+const webhook = new IncomingWebhook(url)
+
+program.on("--help", function() {
+  console.log("")
+  console.log("  Example:")
+  console.log("")
+  console.log("    $ jmp slack 'Hello, I like your app.'")
+  console.log("")
+})
+
+program
+  .arguments("<message>")
+  .description("Send me a message in Slack")
+  .parse(process.argv)
+
+var msg = program.args
+
+var msgStr = String(msg)
+
+if (!msg.length) {
+  console.error("message required")
+  program.help()
+  process.exit(1)
+}
+// Send simple text to the webhook channel
+webhook.send(msgStr, function(err, res) {
+  if (err) {
+    console.log("Error:", err)
+  } else {
+    console.log()
+    console.log("Message sent!")
+  }
+})
--- bin/jmp.js
+++ bin/jmp.js
@@ -5,6 +5,9 @@ const pkg = require("../package.json")
 
 program
   .version(pkg.version)
+  .description("jmp-cli by Jared M. Peterson")
   .command("resume", "print json resume to your terminal")
-  // .command("login", "personalize your experience")
-  .parse(process.argv)
+  .command("slack <message>", "send me a message in slack")
+// .command("login", "personalize your experience") namespace login
+
+program.parse(process.argv)
